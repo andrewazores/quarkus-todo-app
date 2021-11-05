a8e579bed999cd7953268cef1c844f450409e770
Andrew Azores <me@andrewazor.es>
2021-11-05 16:48:34 -0400

    Add API JFR event

--- quarkus-todo/src/main/java/io/quarkus/sample/TodoResource.java
+++ quarkus-todo/src/main/java/io/quarkus/sample/TodoResource.java
@@ -21,6 +21,11 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import io.quarkus.panache.common.Sort;
+import jdk.jfr.Event;
+import jdk.jfr.FlightRecorder;
+import jdk.jfr.Label;
+import jdk.jfr.Name;
+import jdk.jfr.Registered;
 
 
 @Path("/api")
@@ -28,79 +33,152 @@ import io.quarkus.panache.common.Sort;
 @Consumes("application/json")
 public class TodoResource {
 
+    static {
+        FlightRecorder.register(TodoResourceEvent.class);
+    }
+
     @Inject TodoLogger logger;
 
     @OPTIONS
     public Response opt() {
-        return Response.ok().build();
+        TodoResourceEvent evt = new TodoResourceEvent("OPTIONS", -1);
+        try {
+            evt.begin();
+            return Response.ok().build();
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
+            }
+        }
     }
 
     @GET
     public List<Todo> getAll() {
-        return Todo.listAll(Sort.by("order"));
+        TodoResourceEvent evt = new TodoResourceEvent("GET", -1);
+        try {
+            evt.begin();
+            return Todo.listAll(Sort.by("order"));
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
+            }
+        }
     }
 
     @GET
     @Path("/{id}")
     public Todo getOne(@PathParam("id") Long id) {
-        Optional<Todo> entity = Todo.findByIdOptional(id);
-        return entity.orElseThrow(() -> new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND));
+        TodoResourceEvent evt = new TodoResourceEvent("GET", id);
+        try {
+            Optional<Todo> entity = Todo.findByIdOptional(id);
+            return entity.orElseThrow(() -> new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND));
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
+            }
+        }
     }
 
     @POST
     @Transactional
     public Response create(@Valid Todo item) {
-        logger.log("Creating TODO: " + item.title);
-        boolean alreadyExists = false;
-        for (Todo todo : Todo.<Todo>listAll()) {
-            if (Objects.equals(item.id, todo.id)) {
-                alreadyExists = alreadyExists || true;
+        TodoResourceEvent evt = new TodoResourceEvent("POST");
+        try {
+            logger.log("Creating TODO: " + item.title);
+            boolean alreadyExists = false;
+            for (Todo todo : Todo.<Todo>listAll()) {
+                if (Objects.equals(item.id, todo.id)) {
+                    alreadyExists = alreadyExists || true;
+                }
+            }
+            if (alreadyExists) {
+                throw new WebApplicationException(400);
+            }
+            item.persist();
+            evt.id = item.id;
+            logger.log(String.format("Created TODO<%d>: %s", item.id, item.title));
+            return Response.status(Status.CREATED).entity(item).build();
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
             }
         }
-        if (alreadyExists) {
-            throw new WebApplicationException(400);
-        }
-        item.persist();
-        logger.log(String.format("Created TODO<%d>: %s", item.id, item.title));
-        return Response.status(Status.CREATED).entity(item).build();
     }
 
     @PATCH
     @Path("/{id}")
     @Transactional
     public Response update(@Valid Todo todo, @PathParam("id") Long id) {
-        Todo entity = Todo.findById(id);
-        entity.id = id;
-        entity.completed = todo.completed;
-        entity.order = todo.order;
-        entity.title = todo.title;
-        entity.url = todo.url;
-        return Response.ok(entity).build();
+        TodoResourceEvent evt = new TodoResourceEvent("PATCH", id);
+        try {
+            Todo entity = Todo.findById(id);
+            entity.id = id;
+            entity.completed = todo.completed;
+            entity.order = todo.order;
+            entity.title = todo.title;
+            entity.url = todo.url;
+            return Response.ok(entity).build();
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
+            }
+        }
     }
 
     @DELETE
     @Transactional
     public Response deleteCompleted() {
-        Todo.deleteCompleted();
-        return Response.noContent().build();
+        TodoResourceEvent evt = new TodoResourceEvent("DELETE");
+        try {
+            Todo.deleteCompleted();
+            return Response.noContent().build();
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
+            }
+        }
     }
 
     @DELETE
     @Transactional
     @Path("/{id}")
     public Response deleteOne(@PathParam("id") Long id) {
-        logger.log("Deleting ID: " + id);
-        for (Todo todo : Todo.<Todo>listAll()) {
-            if (Objects.equals(id, todo.id)) {
-                todo.delete();
-                todo.flush();
-                logger.log("Deleted ID: " + id);
+        TodoResourceEvent evt = new TodoResourceEvent("DELETE", id);
+        try {
+            logger.log("Deleting ID: " + id);
+            for (Todo todo : Todo.<Todo>listAll()) {
+                if (Objects.equals(id, todo.id)) {
+                    todo.delete();
+                    todo.flush();
+                    logger.log("Deleted ID: " + id);
+                }
+            }
+            return Response.noContent().build();
+            // Optional<Todo> entity = Todo.findByIdOptional(id);
+            // entity.ifPresent(Todo::delete);
+            // return entity.map(todo -> Response.noContent().build()).orElseThrow(() -> new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND));
+        } finally {
+            if (evt.shouldCommit()) {
+                evt.commit();
             }
         }
-        return Response.noContent().build();
-        // Optional<Todo> entity = Todo.findByIdOptional(id);
-        // entity.ifPresent(Todo::delete);
-        // return entity.map(todo -> Response.noContent().build()).orElseThrow(() -> new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND));
+    }
+
+    @Name("io.quarkus.sample.TodoResource")
+    @Label("Todo Resource API Event")
+    @Registered(true)
+    static class TodoResourceEvent extends Event {
+        final String method;
+        long id;
+
+        TodoResourceEvent(String method) {
+            this.method = method;
+        }
+
+        TodoResourceEvent(String method, long id) {
+            this.method = method;
+            this.id = id;
+        }
     }
 
 }
